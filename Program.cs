6255123c84f51141473a1dbb1920ec6b0ad68128
s27747 <s27747@pjwstk.edu.pl>
2025-03-05 14:38:35 +0100

    Dodano metode CalculateAverage

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Modyfikacja 3");
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Średnia: " + CalculateAverage(numbers));
+    }
+
+    static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            return 0;
+        return numbers.Average();
+    }
+}
+
